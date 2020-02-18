3728a20b6fd3a6b7bd095226f54661c585718895
aluno <adsroot.aluno.ifsc.edu.br>
2020-02-18 20:23:06 -0300

    save

--- app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
@@ -3,6 +3,7 @@ package br.ifsc.edu.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("lifeCycleActiv", "onCreate");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifeCycleActiv", "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lifeCycleActiv", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifeCycleActiv", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifeCycleActiv", "onStop");
     }
 }

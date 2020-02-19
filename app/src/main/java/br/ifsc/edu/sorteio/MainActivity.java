1a854d537d45abb384cedc5e747e26039d8f3b47
aluno <adsroot.aluno.ifsc.edu.br>
2020-02-18 22:14:24 -0300

    build view and logic to random number

--- app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
@@ -4,14 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editTextFirst;
+    EditText editTextLast;
+    Button btnRandom;
+
+
+    public void copy(View view){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("lifeCycleActiv", "onCreate");
+
+        editTextFirst = findViewById(R.id.textInputLeft);
+        editTextLast = findViewById(R.id.textInputRight);
+        textView = findViewById(R.id.textViewResultado);
+//
+        btnRandom = findViewById(R.id.btnRandom);
+        //bind function onclick by java
+        btnRandom.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                int min = Integer.parseInt(editTextFirst.getText().toString());
+                int max = Integer.parseInt(editTextLast.getText().toString());
+                int random = (int) (Math.random() *  (max - min  + 1)) + min;
+                textView.setText(Integer.toString(random));
+            }
+        });
+
+
+
     }
 
     @Override
